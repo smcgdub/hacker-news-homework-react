267b6b20f2efbf00bcd1d95482ded157ce4424b1
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-09-20 20:05:46 +0100

    Created NewsCard.js component and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import NewsCard from './components/NewsCard';
 
 export default function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <NewsCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
