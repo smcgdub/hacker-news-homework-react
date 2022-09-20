f65fb6d22f51a69e0856d5c8fba07351c0ea4a5e
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-09-20 19:48:21 +0100

    Created components folder and also Navbar.js file

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="index.html">
+          <span>Hacker News</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
